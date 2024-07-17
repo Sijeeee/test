1dfe2d5d3d277d1e56e55abf73bce00c622a9fdc
Sijeeee <202311905@fit.edu.ph>
2024-07-17 10:12:30 +0800

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "153px";
+    document.getElementById("closebtn").style.display = "block";
+        }
+    function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("closebtn").style.display = "none";
+    }
